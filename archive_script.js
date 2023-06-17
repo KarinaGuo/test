5dae2b160f9d7f9d65079531b6a7c93c31fe624f
KarinaGuo <karinag.work@gmail.com>
2023-06-17 13:10:31 +1000

    test_website

---
+++ archive_script.js
@@ -0,0 +1,71 @@
+// Retrieve necessary elements from the DOM
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const addButton = document.getElementById('add-button');
+const todoList = document.getElementById('todo-list');
+const archivedList = document.getElementById('archived-list');
+
+// Counter to generate unique IDs
+let counter = 1;
+
+// Event listener for form submission
+todoForm.addEventListener('submit', function (e) {
+  e.preventDefault(); // Prevent form submission
+
+  const todoName = todoInput.value.trim(); // Get the trimmed value of the input
+
+  if (todoName !== '') {
+    // Create a new list item
+    const listItem = document.createElement('li');
+    listItem.textContent = todoName;
+
+    // Generate unique ID for the list item
+    const id = 'list-item-' + counter;
+    listItem.setAttribute('id', id);
+    counter++;
+
+    // Create a button to archive the list item
+    const archiveButton = document.createElement('button');
+    archiveButton.textContent = 'Archive';
+    archiveButton.addEventListener('click', function () {
+      // Move the list item to the Archived Lists section
+      listItem.remove();
+      archivedList.appendChild(listItem);
+      archiveButton.remove();
+    });
+
+    // Append the button to the list item
+    listItem.appendChild(archiveButton);
+
+    // Append the list item to the To-Do List section
+    todoList.appendChild(listItem);
+
+    // Clear the input field
+    todoInput.value = '';
+
+    // Create a link between the archived list item and the to-do list item
+    const archivedListItem = document.createElement('li');
+    archivedListItem.innerHTML = `
+      <details>
+        <summary>${todoName}</summary>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+          <!-- Add more tasks here as needed -->
+        </ul>
+      </details>
+    `;
+
+    archivedListItem.addEventListener('click', function () {
+      // Expand or collapse the archived list item
+      archivedListItem.querySelector('details').open = !archivedListItem.querySelector('details').open;
+    });
+
+    // Set the data attribute to link the archived list item with the to-do list item
+    archivedListItem.setAttribute('data-linked-item', id);
+
+    // Append the archived list item to the Archived Lists section
+    archivedList.appendChild(archivedListItem);
+  }
+});
